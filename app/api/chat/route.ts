08fb3d7b341a8aacffe5bfa561cfbc46d0816bd6
Gaurav Nardia <gauravnardia07@gmail.com>
2025-04-01 21:28:03 +0530

    fix api/chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,27 +1,23 @@
 import { client } from "@/lib/server/model";
 import { getSystemPrompt } from "@/lib/server/prompt";
 
-
-
 export async function GET(){
     return  Response.json({ message: "hello openAI" })
- }
+}
 
-export async function POST(req: Request, res: Response){
+export async function POST(req: Request){
     const { messages } = await req.json();
 
-   const stream = await client.responses.create({
+    const stream = await client.responses.create({
         model: "gpt-4o",
         instructions: getSystemPrompt(),
-        input:messages,
+        input: messages,
         max_output_tokens: 8000
     });
 
-
- 
     return new Response(JSON.stringify(stream.output_text), {
         headers: { 'Content-Type': 'application/json' },
         status: 200,
-      });
+    });
 }
 
